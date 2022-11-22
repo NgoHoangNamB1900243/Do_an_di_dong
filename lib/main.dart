50f797ca9e8ec3c96873d60f6210682bb2de8dd8
NgoHoangNamB1900243 <113842747+NgoHoangNamB1900243@users.noreply.github.com>
2022-11-22 09:02:43 +0700

    Xay dung drawer va dinh tuyen theo ten

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 
-import 'ui/product/products_manager.dart';
-import 'ui/product/product_detail_screen.dart';
+import 'ui/products/products_manager.dart';
+import 'ui/products/product_detail_screen.dart';
 
-import 'ui/product/product_overview_screen.dart';
-import 'ui/product/user_product_screen.dart';
+import 'ui/products/products_overview_screen.dart';
+import 'ui/products/user_products_screen.dart';
+
+import 'ui/cart/cart_screen.dart';
+import 'ui/orders/orders_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -27,9 +30,25 @@ class MyApp extends StatelessWidget {
           secondary: Colors.deepOrange,
         ),
       ),
-      home: const SafeArea(
-        child: UserProductScreen(),
-      ),
+      home: const ProductsOverviewScreen(),
+      routes: {
+        CartScreen.routeName: (ctx) => const CartScreen(),
+        OrdersScreen.routeName: (ctx) => const OrdersScreen(),
+        UserProductScreen.routeName: (ctx) => const UserProductScreen(),
+      },
+      onGenerateRoute: (settings) {
+        if (settings.name == ProductDetailScreen.routeName) {
+          final productId = settings.arguments as String;
+          return MaterialPageRoute(
+            builder: (ctx) {
+              return ProductDetailScreen(
+                ProductsManager().findById(productId),
+              );
+            },
+          );
+        }
+        return null;
+      },
     );
   }
 }
